cbdbb8c2d282605100e1ac3fc5f746f8ccdf358c
OBOGWUJOSH <165791807+OBOGWUJOSH@users.noreply.github.com>
2024-07-02 09:44:14 +0100

    Create classTaskORevision.js

---
+++ classTaskORevision.js
@@ -0,0 +1,95 @@
+// kooleve00224
+
+
+
+// const testScores = [44,78,98,56,44,54,33,88,99,77];
+//     let newScores = testScores.filter(function(testScore)
+//  {return testScore > 70;
+// })
+//     console.log(newScores)
+
+
+
+
+// const examGrade = [85,92,78,88,95]
+
+//     let newExamScores = examGrade.map(function(addFiveToScores){
+//         return addFiveToScores + 5;
+//     })
+//     console.log(newExamScores)
+
+
+
+
+
+// const numbers = [2,4,6,8,10]
+
+//     let newNumbers = numbers.map(function(squareOfNumbers){
+//         return squareOfNumbers * squareOfNumbers;
+//     })
+//     console.log(newNumbers)
+
+
+
+
+
+
+// const members = ["Emely", "Jack", "Sophia", "Daniel", "John"]
+// const books = ["book1", "book2", "book3", "book4"]
+
+// function generateAnObjectItem(a,b){
+//     const obj = {}
+//     members.forEach((a, b) =>{
+//     obj[a] = books[b];
+//     })
+//     return obj
+// }
+//     console.log(generateAnObjectItem(members,books));
+
+
+
+
+
+
+// let a = [1, 2, 3, 4];
+// let b = ["ram", "shyam", "sita", "gita","jesus"]
+
+
+// function creatKeyAndValue(a,b){
+//     if (a.length != b.length||a.length != b.length){
+//         return "length not the same";
+//         }let c = {} 
+//         a.forEach((element,elements) =>  {
+//             c[element] = b[elements]
+//         })
+//             return c;
+// }
+
+//     console.log(creatKeyAndValue(a,b));
+
+
+
+
+let student = "jake";
+let classes = "AM"
+let timeOfClasesTime = ["9:00AM", "11:00AM","1:00PM", "3:00PM", "5:00PM"]
+
+function timeOfClasses (a,b){
+
+    timeOfClasesTime.forEach((x,y)=>{
+        if (y.contains("AM")){
+        return a,b;
+        
+    }
+
+
+})
+}
+
+
+        
+
+    console.log( timeOfClasses (timeOfClasesTime,classes));
+    //console.log(timeOfClasesTime.filter(timeOfClasses));
+
+    
